4c9cee013808f0ad0d425d598e7de058ab078cde
enderbarillas <ebarillas@smu.edu>
2015-04-28 12:29:15 -0500

    Changed to tabbed project

---
+++ Explore@SMU/FourthViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FourthViewController.swift
+//  Explore@SMU
+//
+//  Created by ch484-mac5 on 4/28/15.
+//  Copyright (c) 2015 Team B.E.N. All rights reserved.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+    
+}
+
---
+++ Explore@SMU/ThirdViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ThirdViewController.swift
+//  Explore@SMU
+//
+//  Created by ch484-mac5 on 4/28/15.
+//  Copyright (c) 2015 Team B.E.N. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+    
+}
+
