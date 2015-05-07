6cf62e75fd5f9c43fd4031c2511f60654a0a4992
nsliwa <nsliwa@smu.edu>
2015-05-07 03:37:16 -0500

    more model processing tweaks

--- Prediction/initializeDatabase.py
+++ Prediction/initializeDatabase.py
@@ -23,12 +23,12 @@ locations = [
 	# ["Blanton Student Servies", ["Blanton Building Number", "Blanton Centennial Pavilion", "Blanton Cooper Centennial Fountain", "Blanton Entrance", "Blanton Fountain"]],
 	# ["Cox School of Business", ["Cox", "Cox Alumni", "Cox Computer Lab", "Cox Networking", "Cox Quad", "Cox Quad Entrance", "Cox Trade"]],
 	# ["Dedman School of Humanities", ["Dedman", "Dedman Dallas Hall Entrance", "Dedman Dallas Hall Portrait", "Dedman Dallas Hall Seal", "Hughes Trigg Student Center"]],
-	# ["Hughes Trigg Basement", ["Hughes Trigg Cafe 100", "Hughes Trigg Commons", "Hughes Trigg M Lounge", "Hughes Trigg Mail", "Hughes Trigg Mane Course", "Hughes Trigg Peruna Statue", "Hughes Trigg SMU Unbridled"]],
+	["Hughes Trigg Basement", ["Hughes Trigg Cafe 100", "Hughes Trigg Commons", "Hughes Trigg M Lounge", "Hughes Trigg Mail", "Hughes Trigg Mane Course", "Hughes Trigg Peruna Statue", "Hughes Trigg SMU Unbridled"]],
 	# ["Lyle School of Engineering", ["Lyle", "Lyle Ampitheater", "Lyle Dean's Suite", "Lyle Hunt Institute", "Lyle Innovation Gym", "Lyle Junkins", "Lyle Office of Recruitment", "Lyle Seal", "Lyle Wall of Patents"]],
 	# ["Meadows Museum", ["Meadows M Crouching Woman", "Meadows M Figure with Raised Arms", "Meadows M Geometric Mouse II", "Meadows M Horse and Rider", "Meadows M Spirit's Flight", "Meadows M Three Piece Reclining Figure"]], 
 	# ["Meadows School of Arts", ["Meadows", "Meadows Auditorium", "Meadows Creative Computing", "Meadows Entrance Art", "Meadows Entrance Painting", "Meadows Library", "Meadows Lounge", "Meadows Painting", "Meadows Tech Effect"]],
-	["Perkins School of Theology", ["Perkins Chapel", "Perkins Chapel Garden", "Perkins Library Entrance"]],
-	["Residential Commons", ["Residential Commons Armstrong", "Residential Commons Band Hall", "Residential Commons Blue Light", "Residential Commons Crow", "Residential Commons Loyd"]]
+	# ["Perkins School of Theology", ["Perkins Chapel", "Perkins Chapel Garden", "Perkins Library Entrance"]],
+	# ["Residential Commons", ["Residential Commons Armstrong", "Residential Commons Band Hall", "Residential Commons Blue Light", "Residential Commons Crow", "Residential Commons Loyd"]]
 
 
 ]
